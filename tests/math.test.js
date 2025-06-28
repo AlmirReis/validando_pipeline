8662f6faf15647d17e8db5242522934bd7c693ad
Almir Reis <falecomalmir@gmail.com>
2025-06-28 15:57:08 -0300

    Corrigindo pipeline e ajustes de lint

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,8 +1,8 @@
 // tests/math.test.js
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('Math Test', () => {
-  it('2 + 2 deve ser 4', () => {
+describe("Math Test", () => {
+  it("2 + 2 deve ser 4", () => {
     expect(2 + 2).toBe(4);
   });
 });
