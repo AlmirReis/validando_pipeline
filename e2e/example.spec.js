741cdbed442ceaa7a13f4f84ce96680d120df45b
Almir Reis <falecomalmir@gmail.com>
2025-06-28 11:18:23 -0300

    ci: corrigindo erro

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -2,5 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('Acessar página do Playwright e validar título', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  await page.waitForLoadState('load');
   await expect(page).toHaveTitle(/Playwright/);
 });
