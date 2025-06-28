cbfd34b6141ed473dd5894300e8f0a1ca86cab31
Almir Reis <falecomalmir@gmail.com>
2025-06-28 16:18:51 -0300

    ci: correcao

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,8 +1,13 @@
-// tests/math.test.js
 import { describe, it, expect } from "vitest";
 
-describe("Math Test", () => {
-  it("2 + 2 deve ser 4", () => {
-    expect(2 + 2).toBe(4);
+describe("Operações matemáticas", () => {
+  it("Deve somar dois números corretamente", () => {
+    const result = 2 + 3;
+    expect(result).toBe(5);
+  });
+
+  it("Deve multiplicar dois números corretamente", () => {
+    const result = 4 * 5;
+    expect(result).toBe(20);
   });
 });
